67233423e86f42b7e8bb50cd94a9c244ff375bbc
wintyo <wintyo1024@gmail.com>
2021-01-03 17:32:49 +0900

    firebaseのconfigをcommitする
    結局ソースに含まれて隠す意味がないため登録する

---
+++ config/firebase.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyBL4FLiatM0n2R-GfTif5y94dkYwQ85Zz8',
+  authDomain: 'fir-react-chat-8b229.firebaseapp.com',
+  projectId: 'fir-react-chat-8b229',
+  storageBucket: 'fir-react-chat-8b229.appspot.com',
+  messagingSenderId: '687157764341',
+  appId: '1:687157764341:web:d0b2dfac49e23970e265bc',
+};
