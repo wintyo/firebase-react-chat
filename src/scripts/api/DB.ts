b79b14283c1ba340fb3df15a62d5e218d61b5091
wintyo <wintyo1024@gmail.com>
2021-01-03 14:39:17 +0900

    チャット送信の実装

--- src/scripts/api/DB.ts
+++ src/scripts/api/DB.ts
@@ -25,6 +25,35 @@ class DB {
     });
     return data;
   }
+
+  /**
+   * メッセージの送信
+   * @param roomId - ルームID
+   * @param name - 名前
+   * @param message - メッセージ
+   */
+  async sendMessage(roomId: string, name: string, message: string) {
+    await db.collection(`rooms/${roomId}/messages`).add({
+      name,
+      message,
+    });
+  }
+
+  /**
+   * メッセージ一覧の取得
+   * @param roomId - ルームID
+   */
+  async fetchMessages(roomId: string) {
+    const snapshot = await db.collection(`rooms/${roomId}/messages`).get();
+    const data: any[] = [];
+    snapshot.forEach((doc) => {
+      data.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    return data;
+  }
 }
 
 export default new DB();
--- src/scripts/pages/Room.tsx
+++ src/scripts/pages/Room.tsx
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import DB from '~/api/DB';
 
 export default () => {
   const { roomId } = useParams<any>();
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState<any[]>([]);
 
-  return <div>Room {roomId}</div>;
+  useEffect(() => {
+    (async () => {
+      const data = await DB.fetchMessages(roomId);
+      console.log(data);
+      setMessages(data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      Room {roomId}
+      <div>
+        <input
+          type="text"
+          value={name}
+          placeholder="名前"
+          onChange={(event) => {
+            setName(event.currentTarget.value);
+          }}
+        />
+        <br />
+        <input
+          type="text"
+          value={message}
+          placeholder="メッセージ"
+          onChange={(event) => {
+            setMessage(event.currentTarget.value);
+          }}
+        />
+        <br />
+        <button
+          onClick={async () => {
+            await DB.sendMessage(roomId, name, message);
+            setMessage('');
+          }}
+        >
+          送信
+        </button>
+      </div>
+      <ul>
+        {messages.map((message) => (
+          <li key={message.id}>
+            name: {message.name}
+            <br />
+            message: {message.message}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
